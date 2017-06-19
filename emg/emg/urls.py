b741485e860e38524c750cdd58f90e622f0b0502
Ola Tarkowska <olat@ebi.ac.uk>
2017-06-19 22:16:38 +0100

    clean up schema

--- emg/emg/urls.py
+++ emg/emg/urls.py
@@ -3,14 +3,14 @@
 from django.conf.urls import include, url
 
 from rest_framework.schemas import get_schema_view
-from rest_framework.renderers import CoreJSONRenderer
+# from rest_framework.renderers import CoreJSONRenderer
 
 from rest_framework_swagger.views import get_swagger_view
 
 
 schema_view = get_schema_view(
     title='EBI metagenomics API',
-    renderer_classes=[CoreJSONRenderer]
+    # renderer_classes=[CoreJSONRenderer]
 )
 
 docs_schema_view = get_swagger_view(title='EBI metagenomics API')
@@ -19,14 +19,12 @@ docs_schema_view = get_swagger_view(title='EBI metagenomics API')
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
 
-    # url(r'^admin/', admin.site.urls),
+    url(r'^$', schema_view),
 
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 
     url(r'^api/', include('emg_api.urls')),
-
-    url(r'^schema/$', schema_view),
 
     url(r'^docs/$', docs_schema_view),
 
