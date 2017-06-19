9454d8116abe6ddf60ddfc93fef3fb9bd0a9829b
Ola Tarkowska <ola@red-aries.com>
2017-06-19 15:12:31 +0100

    implement first set of urls

--- emg/emg/urls.py
+++ emg/emg/urls.py
@@ -1,21 +1,33 @@
-"""emg URL Configuration
+# -*- coding: utf-8 -*-
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import include, url
 
+from rest_framework.schemas import get_schema_view
+from rest_framework.renderers import CoreJSONRenderer
+
+from rest_framework_swagger.views import get_swagger_view
+
+
+schema_view = get_schema_view(
+    title='EBI metagenomics API',
+    renderer_classes=[CoreJSONRenderer]
+)
+
+docs_schema_view = get_swagger_view(title='EBI metagenomics API')
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+
+    # url(r'^admin/', admin.site.urls),
+
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
+
+    url(r'^api/', include('emg_api.urls')),
+
+    url(r'^schema/$', schema_view),
+
+    url(r'^docs/$', docs_schema_view),
+
 ]
