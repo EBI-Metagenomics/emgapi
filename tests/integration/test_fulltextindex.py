611103d1727a6aad8ac9158ef6b801287990e4a8
Ola Tarkowska <olat@ebi.ac.uk>
2017-07-04 00:01:45 +0100

    add fulltext tests using live server

--- tests/integration/test_fulltextindex.py
+++ tests/integration/test_fulltextindex.py
@@ -24,35 +24,130 @@ from model_mommy import mommy
 from django.core.urlresolvers import reverse
 
 
-@pytest.mark.skip(reason="zero results")
+def create_biomes(count):
+    entries = []
+    for pk in range(1, count+1):
+        entries.append(
+            mommy.prepare(
+                "emg_api.Biome",
+                pk=pk,
+                biome_name="Biome findme",
+                lineage="root:biome:findme"
+            )
+        )
+    for pk in range(count+1, 2*count+1):
+        entries.append(
+            mommy.prepare(
+                "emg_api.Biome",
+                pk=pk,
+                biome_name="Biome hideme",
+                lineage="root:biome:hideme"
+            )
+        )
+    return entries
+
+
+def create_publications(count):
+    entries = []
+    for pk in range(1, count+1):
+        entries.append(
+            mommy.prepare(
+                "emg_api.Publication",
+                pk=pk,
+                pub_title="Publication findme",
+                pub_abstract="abcdefghijklmnoprstuvwxyz"
+            )
+        )
+    for pk in range(count+1, 2*count+1):
+        entries.append(
+            mommy.prepare(
+                "emg_api.Publication",
+                pk=pk,
+                pub_title="Publication hide",
+                pub_abstract="abcdefghijklmnoprstuvwxyz"
+            )
+        )
+    return entries
+
+
+def create_studies(count):
+    entries = []
+    for pk in range(1, count+1):
+        _biome = mommy.make('emg_api.Biome', pk=pk)
+        entries.append(
+            mommy.prepare(
+                "emg_api.Study",
+                pk=pk,
+                biome=_biome,
+                study_name="Study findme",
+                study_abstract="abcdefghijklmnoprstuvwxyz"
+            )
+        )
+    for pk in range(count+1, 2*count+1):
+        _biome = mommy.make('emg_api.Biome', pk=pk)
+        entries.append(
+            mommy.prepare(
+                "emg_api.Study",
+                pk=pk,
+                biome=_biome,
+                study_name="Study hide",
+                study_abstract="abcdefghijklmnoprstuvwxyz"
+            )
+        )
+    return entries
+
+
+def create_samples(count):
+    entries = []
+    for pk in range(1, count+1):
+        _biome = mommy.make('emg_api.Biome', pk=pk)
+        _study = mommy.make('emg_api.Study', pk=pk, biome=_biome)
+        entries.append(
+            mommy.prepare(
+                "emg_api.Sample",
+                pk=pk,
+                biome=_biome,
+                study=_study,
+                sample_name="Sample findme"
+            )
+        )
+    for pk in range(count+1, 2*count+1):
+        _biome = mommy.make('emg_api.Biome', pk=pk)
+        _study = mommy.make('emg_api.Study', pk=pk, biome=_biome)
+        entries.append(
+            mommy.prepare(
+                "emg_api.Sample",
+                pk=pk,
+                biome=_biome,
+                study=_study,
+                sample_name="Sample hideme"
+            )
+        )
+    return entries
+
+
 class TestFullTextIndexAPI(object):
 
     @pytest.mark.parametrize(
-        '_model, _view, search_term, results',
+        '_model, _view, search_term, search_attr, counts',
         [
-            ('Publication', 'publications', 'title', 4),
+            ('Biome', 'biomes', 'findme', 'biome_name', 5),
+            ('Study', 'studies', 'findme', 'study_name', 5),
+            ('Sample', 'samples', 'findme', 'sample_name', 5),
+            ('Publication', 'publications', 'findme', 'pub_title', 5),
         ]
     )
     @pytest.mark.django_db
-    def test_search(self, client, _model, _view, search_term, results):
+    def test_search(self, live_server, client,
+                    _model, _view, search_term, search_attr, counts):
         klass = getattr(importlib.import_module("emg_api.models"), _model)
-
-        model_name = "emg_api.%s" % _model
-
-        entries = []
-        for pk in range(1, results+1):
-            entries.append(
-                mommy.prepare(
-                    model_name,
-                    pub_title="publication title foo bar",
-                    pub_abstract="abcdefghijklmnoprstuvwxyz"
-                )
-            )
+        entries = globals()["create_%s" % _view](counts)
         klass.objects.bulk_create(entries)
+        assert len(klass.objects.all()) == 2*counts
 
         view_name = "%s-list" % _view
         qs = urlencode({'search': search_term})
-        url = "%s?%s" % (reverse(view_name), qs)
+        url = "%s%s?%s" % (live_server.url, reverse(view_name), qs)
         response = client.get(url)
         assert response.status_code == 200
         rsp = response.json()
@@ -60,11 +155,12 @@ class TestFullTextIndexAPI(object):
         # Meta
         assert rsp['meta']['pagination']['page'] == 1
         assert rsp['meta']['pagination']['pages'] == 1
-        assert rsp['meta']['pagination']['count'] == results
+        assert rsp['meta']['pagination']['count'] == counts
 
         # Data
-        assert len(rsp['data']) == results
+        assert len(rsp['data']) == counts
 
         for d in rsp['data']:
             assert d['type'] == _model
-            assert search_term in d['attributes']['pub_title']
+            assert d['attributes'][search_attr] == "%s findme" % _model
+            assert not d['attributes'][search_attr] == "%s hideme" % _model
