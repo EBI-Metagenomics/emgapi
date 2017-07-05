405d5ba10f9b478669276b084abdc67dae7758d5
Ola Tarkowska <olat@ebi.ac.uk>
2017-07-05 12:02:35 +0100

    allow session base authentication

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 
 Requirements:
 
- - MySQL 5.6 or 5.7
+ - Python 3.5 or 3.6
  - Django 1.11
+ - MySQL 5.6 or 5.7
+
 
 Copyright (c) 2017 EMBL - European Bioinformatics Institute
--- emg/emg_api/admin.py
+++ emg/emg_api/admin.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from emg_api import models as emg_models
+
+admin.site.register(emg_models.Biome)
+admin.site.register(emg_models.Study)
+admin.site.register(emg_models.Sample)
+admin.site.register(emg_models.Run)
+admin.site.register(emg_models.Publication)
