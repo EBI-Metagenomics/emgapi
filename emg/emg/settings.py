85509d5bf1a2f19bf91ffe21b1f78cd67208bf60
Ola Tarkowska <olat@ebi.ac.uk>
2017-07-05 16:07:07 +0100

    change default permissions

--- emg/emg/settings.py
+++ emg/emg/settings.py
@@ -297,7 +297,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-         'rest_framework.permissions.AllowAny',
+         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
          # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
 }
