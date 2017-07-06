0aec81696e6534f97a3d065020808fd54f7775b0
Ola Tarkowska <olat@ebi.ac.uk>
2017-07-06 15:18:39 +0100

    add biome top10 test

--- tests/integration/test_biome.py
+++ tests/integration/test_biome.py
@@ -16,14 +16,50 @@
 
 
 # import pytest
+import random
 
 from django.core.urlresolvers import reverse
 from rest_framework.test import APITestCase
 
+from model_mommy import mommy
+
 
 class TestBiomeAPI(APITestCase):
 
+    def setUp(self):
+        self.data = {}
+        self.data['biomes'] = []
+        _biome = [
+            'root',
+            'root:foo', 'root:foo2', 'root:foo3', 'root:foo4', 'root:foo5',
+            'root:foo:bar', 'root:foo:bar2', 'root:foo:bar3',
+            'root:foo:bar4', 'root:foo:bar4'
+        ]
+        for b in _biome:
+            self.data['biomes'].append(
+                mommy.make(
+                    'emg_api.Biome',
+                    biome_name=b,
+                    lineage=b,
+                    pk=(_biome.index(b)+1))
+            )
+        self.data['studies'] = []
+        for pk in range(1, 51):
+            self.data['studies'].append(
+                mommy.make(
+                    'emg_api.Study',
+                    biome=random.sample(self.data['biomes'], 1)[0],
+                    pk=pk,
+                    accession="SRP{:0>3}".format(pk),
+                    is_public=1
+                )
+            )
+
     def test_default(self):
-        url = reverse('biomes-list')
+        url = reverse('biomes-top10')
         response = self.client.get(url)
         assert response.status_code == 200
+        rsp = response.json()
+
+        # Data
+        assert len(rsp['data']) == 10
