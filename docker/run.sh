7fe40d05eecf098145039392841977e88fbcaa6f
Ola Tarkowska <olat@ebi.ac.uk>
2017-08-12 19:30:00 +0100

    improve deployment with miniconda

--- docker/run.sh
+++ docker/run.sh
@@ -4,8 +4,8 @@ set -eux
 
 entryPoint=${1:-}
 srcDir=${SRCDIR:-"${HOME}/src"}
-condaDir=${CONDADIR:-"${HOME}/conda"}
-
+condaDir=${CONDADIR:-"${HOME}/miniconda"}
+pythonVer=${PYTHON_VERSION:-"3.6"}
 
 check_conda() {
   if [[ -f "/etc/profile.d/conda.sh" ]]; then
@@ -16,10 +16,18 @@ check_conda() {
 }
 
 create_venv() {
-  wget --quiet https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86_64.sh -O miniconda.sh
+  wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
   /bin/bash miniconda.sh -b -p $condaDir
   export PATH=$condaDir/bin:$PATH
   rm -rf miniconda.sh
+  hash -r
+  conda update -q conda
+  conda info -a
+
+  # TODO: wait until 4.4 https://github.com/conda/conda/issues/3200
+  #conda create -q -n test-environment python=$pythonVer
+  #source activate test-environment
+
 }
 
 is_db_running() {
@@ -61,10 +69,8 @@ install() {
 }
 
 install_docker() {
-  create_venv
-  python -V
+  install
   is_db_running "-u root -h mysql -P 3306"
-  pip install "git+git://github.com/ola-t/django-rest-framework-json-api@develop#egg=djangorestframework-jsonapi"
 }
 
 docker() {
@@ -78,6 +84,7 @@ travis() {
   #     '/home/travis/build/ola-t/ebi-metagenomics-api/staticfiles/'
   install_src
   cd $srcDir
+  python -V
   python setup.py test
 }
 
