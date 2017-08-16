1443d53e34a14cd7955ff0ed1615cda81e894c43
Ola Tarkowska <olat@ebi.ac.uk>
2017-08-16 14:36:45 +0100

    improve annotation views and update plugin to populate annotations

--- emgapimetadata/models.py
+++ emgapimetadata/models.py
@@ -20,24 +20,25 @@ import mongoengine
 class Annotation(mongoengine.Document):
 
     accession = mongoengine.StringField(
-        required=True, max_length=20, unique_with=('description'))
+        primary_key=True, required=True,
+        max_length=20, unique_with=('description'))
     description = mongoengine.StringField(required=True, max_length=255)
     lineage = mongoengine.StringField(required=True, max_length=255)
 
 
-class RunAnnotation(mongoengine.EmbeddedDocument):
+class AnalysisJobAnnotation(mongoengine.EmbeddedDocument):
 
     annotation = mongoengine.ReferenceField(Annotation)
     count = mongoengine.IntField(required=True)
 
 
-class Run(mongoengine.Document):
+class AnalysisJob(mongoengine.Document):
 
     accession = mongoengine.StringField(
-        required=True, max_length=20,
-        unique_with=('pipeline_version'))
+        primary_key=True, required=True,
+        max_length=20, unique_with=('pipeline_version'))
     pipeline_version = mongoengine.StringField(
         required=True, max_length=20,
         unique_with=('accession'))
     annotations = mongoengine.EmbeddedDocumentListField(
-        RunAnnotation, required=False)
+        AnalysisJobAnnotation, required=False)
