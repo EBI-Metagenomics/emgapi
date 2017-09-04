d7700ecf8759fae1132b0b628896e165a708ed69
Ola Tarkowska <olat@ebi.ac.uk>
2017-09-04 11:33:36 +0100

    refactor annotation plugin: - add abstract annotation class - distinguish subclasses for go and ipr - move annotations related links to analysis job - clean up serializers

--- emgapimetadata/views.py
+++ emgapimetadata/views.py
@@ -35,26 +35,107 @@ from . import models as m_models
 logger = logging.getLogger(__name__)
 
 
-class AnnotationViewSet(m_viewset.ReadOnlyModelViewSet):
+class GoTermViewSet(m_viewset.ReadOnlyModelViewSet):
 
-    serializer_class = m_serializers.AnnotationSerializer
+    serializer_class = m_serializers.GoTermSerializer
 
     lookup_field = 'accession'
     lookup_value_regex = '[a-zA-Z0-9\:]+'
 
     def get_queryset(self):
-        return m_models.Annotation.objects.all()
+        return m_models.GoTerm.objects.all()
 
     def get_object(self):
         accession = self.kwargs.get('accession', None)
-        return m_models.Annotation.objects(accession=accession).first()
+        return m_models.GoTerm.objects(accession=accession).first()
 
     def get_serializer_class(self):
-        return super(AnnotationViewSet, self).get_serializer_class()
+        return super(GoTermViewSet, self).get_serializer_class()
 
 
-class AnnotationRunRelationshipViewSet(mixins.ListModelMixin,
-                                       m_viewset.GenericViewSet):
+class InterProTermViewSet(m_viewset.ReadOnlyModelViewSet):
+
+    serializer_class = m_serializers.InterProTermSerializer
+
+    lookup_field = 'accession'
+    lookup_value_regex = '[a-zA-Z0-9\:]+'
+
+    def get_queryset(self):
+        return m_models.InterProTerm.objects.all()
+
+    def get_object(self):
+        accession = self.kwargs.get('accession', None)
+        return m_models.InterProTerm.objects(accession=accession).first()
+
+    def get_serializer_class(self):
+        return super(InterProTermViewSet, self).get_serializer_class()
+
+
+class GoTermRunRelationshipViewSet(mixins.ListModelMixin,
+                                   m_viewset.GenericViewSet):
+
+    serializer_class = emg_serializers.AnalysisSerializer
+
+    # filter_class = emg_filters.RunFilter
+
+    filter_backends = (
+        # DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    )
+
+    ordering_fields = (
+        'accession',
+    )
+
+    ordering = ('-accession',)
+
+    search_fields = (
+        'instrument_platform',
+        'instrument_model',
+        '@sample__metadata__var_val_ucv',
+    )
+
+    lookup_field = 'accession'
+
+    def get_queryset(self):
+        annotation = get_object_or_404(
+            m_models.GoTerm.objects,
+            accession=self.kwargs[self.lookup_field])
+        run_ids = m_models.AnalysisJobGoTerm.objects \
+            .filter(go_terms__go_term=annotation.pk) \
+            .only('accession', 'pipeline_version')
+        run_ids2 = m_models.AnalysisJobGoSlimTerm.objects \
+            .filter(go_slim__go_term=annotation.pk) \
+            .only('accession', 'pipeline_version')
+        run_ids = [str(r.accession) for r in run_ids]
+        run_ids.extend([str(r.accession) for r in run_ids2])
+        queryset = emg_models.AnalysisJob.objects \
+            .filter(accession__in=run_ids) \
+            .available(self.request) \
+            .select_related(
+                'sample',
+                'analysis_status',
+                'experiment_type'
+            )
+        return queryset
+
+    def get_serializer_class(self):
+        return emg_serializers.AnalysisSerializer
+
+    def list(self, request, accession, *args, **kwargs):
+        """
+        Retrieves list of runs for the given sample accession
+        Example:
+        ---
+        `/annotations/GO:0001/runs`
+        """
+        return super(GoTermRunRelationshipViewSet, self) \
+            .list(request, *args, **kwargs)
+
+
+class InterProTermRunRelationshipViewSet(mixins.ListModelMixin,
+                                         m_viewset.GenericViewSet):
 
     serializer_class = emg_serializers.RunSerializer
 
@@ -82,10 +163,10 @@ class AnnotationRunRelationshipViewSet(mixins.ListModelMixin,
 
     def get_queryset(self):
         annotation = get_object_or_404(
-            m_models.Annotation.objects,
+            m_models.InterProTerm.objects,
             accession=self.kwargs[self.lookup_field])
-        run_ids = m_models.AnalysisJob.objects \
-            .filter(annotations__annotation=annotation.pk) \
+        run_ids = m_models.AnalysisJobInterProTerm.objects \
+            .filter(go_terms__go_term=annotation.pk) \
             .only('accession')
         run_ids = [str(r.accession) for r in run_ids]
         queryset = emg_models.Run.objects \
@@ -108,15 +189,15 @@ class AnnotationRunRelationshipViewSet(mixins.ListModelMixin,
         ---
         `/annotations/GO0001/runs`
         """
-        return super(AnnotationRunRelationshipViewSet, self) \
+        return super(InterProTermRunRelationshipViewSet, self) \
             .list(request, *args, **kwargs)
 
 
-class AnalysisAnnotationRelViewSet(emg_mixins.MultipleFieldLookupMixin,
-                                   mixins.ListModelMixin,
-                                   m_viewset.GenericViewSet):
+class AnalysisGoTermRelViewSet(emg_mixins.MultipleFieldLookupMixin,
+                               mixins.ListModelMixin,
+                               m_viewset.GenericViewSet):
 
-    serializer_class = m_serializers.RetriveAnnotationSerializer
+    serializer_class = m_serializers.GoTermRetriveSerializer
 
     lookup_fields = ('accession', 'release_version')
 
@@ -137,17 +218,127 @@ class AnalysisAnnotationRelViewSet(emg_mixins.MultipleFieldLookupMixin,
         `/runs/ERR1385375/3.0/annotations`
         """
 
-        analysis = m_models.AnalysisJob.objects.filter(
+        analysis = m_models.AnalysisJobGoTerm.objects.filter(
             accession=accession, pipeline_version=release_version).first()
 
         ann_ids = []
         if analysis is not None:
-            ann_ids = [a.annotation.pk for a in analysis.annotations]
+            ann_ids = [a.go_term.pk for a in analysis.go_terms]
             ann_counts = {
-                a.annotation.pk: a.count for a in analysis.annotations
+                a.go_term.pk: a.count for a in analysis.go_terms
             }
 
-        queryset = m_models.Annotation.objects.filter(pk__in=ann_ids)
+        queryset = m_models.GoTerm.objects.filter(pk__in=ann_ids)
+
+        page = self.paginate_queryset(queryset)
+        for p in page:
+            p.count = ann_counts[p.pk]
+        if page is not None:
+            serializer = self.get_serializer(
+                page,
+                many=True,
+                context={'request': request}
+            )
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(
+            queryset,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+
+
+class AnalysisGoSlimRelViewSet(emg_mixins.MultipleFieldLookupMixin,
+                               mixins.ListModelMixin,
+                               m_viewset.GenericViewSet):
+
+    serializer_class = m_serializers.GoTermRetriveSerializer
+
+    lookup_fields = ('accession', 'release_version')
+
+    def get_queryset(self):
+        return emg_models.AnalysisJob.objects \
+            .available(self.request) \
+            .select_related(
+                'sample',
+                'pipeline',
+                'analysis_status',
+            )
+
+    def list(self, request, accession, release_version, *args, **kwargs):
+        """
+        Retrieves run for the given accession and pipeline version
+        Example:
+        ---
+        `/runs/ERR1385375/3.0/annotations`
+        """
+
+        analysis = m_models.AnalysisJobGoSlimTerm.objects.filter(
+            accession=accession, pipeline_version=release_version).first()
+
+        ann_ids = []
+        if analysis is not None:
+            ann_ids = [a.go_term.pk for a in analysis.goslim]
+            ann_counts = {
+                a.go_term.pk: a.count for a in analysis.goslim
+            }
+
+        queryset = m_models.GoTerm.objects.filter(pk__in=ann_ids)
+
+        page = self.paginate_queryset(queryset)
+        for p in page:
+            p.count = ann_counts[p.pk]
+        if page is not None:
+            serializer = self.get_serializer(
+                page,
+                many=True,
+                context={'request': request}
+            )
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(
+            queryset,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+
+
+class AnalysisInterProTermRelViewSet(emg_mixins.MultipleFieldLookupMixin,
+                                     mixins.ListModelMixin,
+                                     m_viewset.GenericViewSet):
+
+    serializer_class = m_serializers.InterProTermRetriveSerializer
+
+    lookup_fields = ('accession', 'release_version')
+
+    def get_queryset(self):
+        return emg_models.AnalysisJob.objects \
+            .available(self.request) \
+            .select_related(
+                'sample',
+                'pipeline',
+                'analysis_status',
+            )
+
+    def list(self, request, accession, release_version, *args, **kwargs):
+        """
+        Retrieves run for the given accession and pipeline version
+        Example:
+        ---
+        `/runs/ERR1385375/3.0/annotations`
+        """
+
+        analysis = m_models.AnalysisJobInterProTerm.objects.filter(
+            accession=accession, pipeline_version=release_version).first()
+
+        ann_ids = []
+        if analysis is not None:
+            ann_ids = [a.go_term.pk for a in analysis.go_terms]
+            ann_counts = {
+                a.go_term.pk: a.count for a in analysis.go_terms
+            }
+
+        queryset = m_models.InterProTerm.objects.filter(pk__in=ann_ids)
 
         page = self.paginate_queryset(queryset)
         for p in page:
