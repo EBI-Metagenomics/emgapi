fd740d4a777d4580c5fc49778bbd903640124b83
Ola Tarkowska <olat@ebi.ac.uk>
2017-10-07 21:02:46 +0100

    migrate collections to new model

---
+++ tests/integration/test_annotations_migrate.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 EMBL - European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+import os
+
+from django.core.urlresolvers import reverse
+from django.core.management import call_command
+
+from rest_framework import status
+
+# import fixtures
+from test_utils.emg_fixtures import *  # noqa
+
+
+@pytest.mark.usefixtures('mongodb')
+@pytest.mark.django_db
+class TestAnnotationsMigration(object):
+
+    def test_migration(self, client, run, mongodb):
+        call_command('import_summary', 'ABC01234',
+                     os.path.dirname(os.path.abspath(__file__)),
+                     suffix='.go_slim')
+        call_command('import_summary', 'ABC01234',
+                     os.path.dirname(os.path.abspath(__file__)),
+                     suffix='.go')
+        call_command('import_summary', 'ABC01234',
+                     os.path.dirname(os.path.abspath(__file__)),
+                     suffix='.ipr')
+        call_command('migrate_annotations', 'ABC01234')
+
+        # drop old collections
+        mongodb['testdb'].drop_collection("analysis_job_go_term")
+        mongodb['testdb'].drop_collection("analysis_job_interpro_identifier")
+
+        url = reverse("emgapi:runs-pipelines-goslim-list",
+                      args=['ABC01234', '1.0'])
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        rsp = response.json()
+        assert len(rsp['data']) == 3
+
+        expected = ['GO:0006810', 'GO:0008152', 'GO:0055114']
+        ids = [a['id'] for a in rsp['data']]
+        assert ids == expected
+
+        url = reverse("emgapi:runs-pipelines-goterms-list",
+                      args=['ABC01234', '1.0'])
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        rsp = response.json()
+        assert len(rsp['data']) == 6
+
+        expected = ['GO:0005975', 'GO:0006412', 'GO:0006810',
+                    'GO:0008152', 'GO:0009058', 'GO:0055114']
+        ids = [a['id'] for a in rsp['data']]
+        assert ids == expected
+
+        url = reverse("emgapi:runs-pipelines-interpro-list",
+                      args=['ABC01234', '1.0'])
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        rsp = response.json()
+        assert len(rsp['data']) == 6
+
+        expected = ['IPR006882', 'IPR007875', 'IPR008659',
+                    'IPR015072', 'IPR024581', 'IPR033393']
+        ids = [a['id'] for a in rsp['data']]
+        assert ids == expected
