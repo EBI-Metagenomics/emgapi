d3a2de6912ca24f2d3892926d0c6c8934acf82c8
Ola Tarkowska <olat@ebi.ac.uk>
2017-10-10 11:16:16 +0100

    fix storing analysis result annotations for multiple pipeline versions

--- tests/integration/path/version_1.0/ABC_FASTQ/ABC_FASTQ_summary.go
+++ tests/integration/path/version_1.0/ABC_FASTQ/ABC_FASTQ_summary.go
@@ -1,6 +1,6 @@
-"GO:0055114","oxidation-reduction process","biological_process",1234
-"GO:0008152","metabolic process","biological_process",5633
-"GO:0006810","transport","biological_process",542
-"GO:0006412","translation","biological_process",532
-"GO:0009058","biosynthetic process","biological_process",787
-"GO:0005975","carbohydrate metabolic process","biological_process",452
+"GO:0030170","pyridoxal phosphate binding","molecular_function","2885"
+"GO:0019842","vitamin binding","molecular_function","1377"
+"GO:0030246","carbohydrate binding","molecular_function","230"
+"GO:0046906","tetrapyrrole binding","molecular_function","2326"
+"GO:0043167","ion binding","molecular_function","32220"
+"GO:0005515","protein binding","molecular_function","6016"
---
+++ tests/integration/path/version_2.0/ABC_FASTQ/ABC_FASTQ_summary.go
@@ -0,0 +1,6 @@
+"GO:0005575","cellular component","cellular_component","67"
+"GO:0005576","extracellular region","cellular_component","37"
+"GO:0005618","cell wall","cellular_component","12"
+"GO:0009276","Gram-negative-bacterium-type cell wall","cellular_component","0"
+"GO:0016020","membrane","cellular_component","14146"
+"GO:0019867","outer membrane","cellular_component","504"
