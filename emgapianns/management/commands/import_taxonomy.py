8d4e1cedfab274f6171fff959ba9348a4218679b
Ola Tarkowska <olat@ebi.ac.uk>
2017-10-13 17:07:16 +0100

    finish taxonomy import script

--- emgapianns/management/commands/import_taxonomy.py
+++ emgapianns/management/commands/import_taxonomy.py
@@ -12,6 +12,14 @@ from ..lib import EMGBaseCommand
 logger = logging.getLogger(__name__)
 
 
+ORGANISM_PREFIX = {
+    '1.0': ['k', 'p', 'c', 'o', 'f', 'g', 's'],
+    '2.0': ['k', 'p', 'c', 'o', 'f', 'g', 's'],
+    '3.0': ['k', 'p', 'c', 'o', 'f', 'g', 's'],
+    '4.0': ['sk', 'k', 'p', 'c', 'o', 'f', 'g', 's'],
+}
+
+
 class Command(EMGBaseCommand):
 
     def add_arguments(self, parser):
@@ -36,15 +44,14 @@ class Command(EMGBaseCommand):
                 logger.info("Found: %s" % _f)
                 with open(_f) as csvfile:
                     reader = csv.reader(csvfile, delimiter='\t')
-                    self.load_organism_from_summary_file(
-                        reader, obj
-                    )
+                    self.load_organism_from_summary_file(reader, obj)
             else:
                 logger.error("Path %r exist. No Taxonomy SKIPPING!" % res)
         else:
             logger.error("Path %r doesn't exist. SKIPPING!" % res)
 
     def load_organism_from_summary_file(self, reader, obj):  # noqa
+
         try:
             run = m_models.AnalysisJobTaxonomy.objects \
                 .get(pk=str(obj.job_id))
@@ -52,12 +59,48 @@ class Command(EMGBaseCommand):
             run = m_models.AnalysisJobTaxonomy()
         run.analysis_id = str(obj.job_id)
         run.accession = obj.accession
-        run.pipeline_version = obj.pipeline.release_version
+        version = obj.pipeline.release_version
+        run.pipeline_version = version
 
+        new_orgs = list()
+        orgs = []
         for row in reader:
-            try:
-                row[0].lower()
-            except KeyError:
-                pass
+            count = row[0]
+            lineage = row[1:]
+            if len(lineage) > 1:
+                name = lineage[-1]
+                ancestors = lineage[0:-1]
+                parent = ancestors[-1]
+                prefix = ORGANISM_PREFIX[version][len(ancestors)]
             else:
-                pass
+                name = lineage[0] if len(lineage) > 0 else "Unassigned"
+                ancestors = []
+                parent = None
+                prefix = ORGANISM_PREFIX[version][len(ancestors)]
+            organism = None
+            try:
+                organism = m_models.Organism.objects.get(pk=lineage)
+            except m_models.Organism.DoesNotExist:
+                organism = m_models.Organism(
+                    lineage=",".join(lineage), name=name, parent=parent,
+                    ancestors=ancestors, prefix=prefix
+                )
+                new_orgs.append(organism)
+            if organism is not None:
+                orgs.append(organism)
+                rorg = m_models.AnalysisJobOrganism(
+                    count=count,
+                    organism=organism
+                )
+                run.taxonomy_ssu.append(rorg)
+
+        if len(orgs) > 0:
+            logger.info(
+                "Total %d Organisms for Run: %s %s" % (
+                    len(orgs), obj.accession, version))
+            if len(new_orgs) > 0:
+                m_models.Organism.objects.insert(new_orgs)
+                logger.info(
+                    "Created %d new Organisms" % len(new_orgs))
+            run.save()
+            logger.info("Saved Run %r" % run)
