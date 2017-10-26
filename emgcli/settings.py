c5163dd4b19289af3a67b66de1b905a1dc1eff6f
Ola Tarkowska <olat@ebi.ac.uk>
2017-10-26 18:23:49 +0100

    fix run endpoint, update tests

--- emgcli/settings.py
+++ emgcli/settings.py
@@ -266,15 +266,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'en-gb'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
-USE_TZ = True
+USE_TZ = False
 
 # Django Rest Framewrk
 
