11bb696f562f222c370eeadaedba16b088fa3a9b
Ola Tarkowska <olat@ebi.ac.uk>
2017-11-13 11:06:19 +0000

    clean up settings

--- emgcli/settings.py
+++ emgcli/settings.py
@@ -317,8 +317,8 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
