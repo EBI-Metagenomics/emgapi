735cc0053c54ef37e6337b1a6263441e1674db8a
Ola Tarkowska <olat@ebi.ac.uk>
2017-12-04 11:02:59 +0000

    fix organisms url regex

--- emgapianns/urls.py
+++ emgapianns/urls.py
@@ -53,13 +53,13 @@ mongo_router.register(
 )
 
 mongo_router.register(
-    r'annotations/organisms/(?P<lineage>.*)',
+    r'annotations/organisms/(?P<lineage>[^/]+)',
     m_views.OrganismTreeViewSet,
     base_name='organisms-children'
 )
 
 mongo_router.register(
-    (r'annotations/organisms/(?P<lineage>.*)/'
+    (r'annotations/organisms/(?P<lineage>[^/]+)/'
      r'analysis'),
     m_views.OrganismAnalysisRelationshipViewSet,
     base_name='organisms-analysis'
