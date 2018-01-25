908cf33297b1d12525d213765b7380b48c28cf2b
Ola Tarkowska <olat@ebi.ac.uk>
2018-01-25 16:52:06 +0000

    update all pipeline versions

--- emgapi/download.py
+++ emgapi/download.py
@@ -1,14 +1,41 @@
 DOWNLOAD_REF = {
+    '1.0': {
+        # functional result files
+        'InterPro.tsv': {
+            'display': 'InterProMatches',
+            'real_suffix': 'summary', 'real_ext': 'ipr',
+            'suffix': 'InterPro', 'ext': 'tsv'
+        },
+        'GO.csv': {
+            'display': 'CompleteGOAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go',
+            'suffix': 'GO', 'ext': 'csv'
+        },
+        'GOslim.csv': {
+            'display': 'GOSlimAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go_slim',
+            'suffix': 'GOslim', 'ext': 'csv'
+        },
+    },
     '2.0': {
-
-        # sequence data files
-        # 'PROCESSED_READS': { },
-        # 'READS_WITH_PREDICTED_CDS': { },
-        # 'READS_WITH_MATCHES_FASTA': { },
-        # 'READS_WITHOUT_MATCHES_FASTA': { },
-        # 'PREDICTED_CDS': { },
-        # 'PREDICTED_CDS_WITHOUT_ANNOTATION': { },
-        # 'PREDICTED_ORF_WITHOUT_ANNOTATION': { },
+        # functional result files
+        'InterPro.tsv': {
+            'display': 'InterProMatches',
+            'real_suffix': 'summary', 'real_ext': 'ipr',
+            'suffix': 'InterPro', 'ext': 'tsv'
+        },
+        'GO.csv': {
+            'display': 'CompleteGOAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go',
+            'suffix': 'GO', 'ext': 'csv'
+        },
+        'GOslim.csv': {
+            'display': 'GOSlimAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go_slim',
+            'suffix': 'GOslim', 'ext': 'csv'
+        },
+    },
+    '3.0': {
 
         # functional result files
         'InterPro.tsv': {
@@ -27,14 +54,45 @@ DOWNLOAD_REF = {
             'suffix': 'GOslim', 'ext': 'csv'
         },
 
-        # taxonomy result files
-        # 'R_RNA_5S_FASTA': { },
-        # 'R_RNA_16S_FASTA': { },
-        # 'R_RNA_23S_FASTA': { },
-        # 'OTU_TABLE': { },
-        # 'HDF5_BIOM': { },
-        # 'JSON_BIOM': { },
-        # 'PRUNED_TREE': { },
+    },
+    '4.0': {
 
-    }
+        # functional result files
+        'InterPro.tsv': {
+            'display': 'InterProMatches',
+            'real_suffix': 'summary', 'real_ext': 'ipr',
+            'suffix': 'InterPro', 'ext': 'tsv'
+        },
+        'GO.csv': {
+            'display': 'CompleteGOAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go',
+            'suffix': 'GO', 'ext': 'csv'
+        },
+        'GOslim.csv': {
+            'display': 'GOSlimAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go_slim',
+            'suffix': 'GOslim', 'ext': 'csv'
+        },
+
+    },
+    '4.1': {
+
+        # functional result files
+        'InterPro.tsv': {
+            'display': 'InterProMatches',
+            'real_suffix': 'summary', 'real_ext': 'ipr',
+            'suffix': 'InterPro', 'ext': 'tsv'
+        },
+        'GO.csv': {
+            'display': 'CompleteGOAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go',
+            'suffix': 'GO', 'ext': 'csv'
+        },
+        'GOslim.csv': {
+            'display': 'GOSlimAnnotation',
+            'real_suffix': 'summary', 'real_ext': 'go_slim',
+            'suffix': 'GOslim', 'ext': 'csv'
+        },
+
+    },
 }
