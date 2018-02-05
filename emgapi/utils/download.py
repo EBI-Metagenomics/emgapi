d4c2c7f7802ee24e32448be8d2554540a20a9f56
Ola Tarkowska <olat@ebi.ac.uk>
2018-02-05 11:38:04 +0000

    add mapping for otu

--- emgapi/utils/download.py
+++ emgapi/utils/download.py
@@ -1,5 +1,35 @@
 DOWNLOAD_REF = {
     '1.0': {
+
+        # taxonomy result files
+        # 'R_RNA_5S_FASTA': { },
+        # 'R_RNA_16S_FASTA': { },
+        # 'R_RNA_23S_FASTA': { },
+        'otu_table.tsv': {
+            'display': 'OTUTable',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table', 'real_ext': 'txt',
+            'suffix': 'otu_table', 'ext': 'tsv'
+        },
+        'otu_table_hdf5.biom': {
+            'display': 'OTUTableHDF5',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_hdf5', 'real_ext': 'biom',
+            'suffix': 'otu_table_hdf5', 'ext': 'biom'
+        },
+        'otu_table_json.biom': {
+            'display': 'OTUTableJSON',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_json', 'real_ext': 'biom',
+            'suffix': 'otu_table_json', 'ext': 'biom'
+        },
+        'pruned.tree': {
+            'display': 'PrunedTree',
+            'subdir': 'cr_otus',
+            'real_suffix': 'pruned', 'real_ext': 'tree',
+            'suffix': 'pruned', 'ext': 'tree'
+        },
+
         # functional result files
         'InterPro.tsv': {
             'display': 'InterProMatches',
@@ -18,6 +48,44 @@ DOWNLOAD_REF = {
         },
     },
     '2.0': {
+        # sequence data files
+        # 'PROCESSED_READS': {},
+        # 'READS_WITH_PREDICTED_CDS': { },
+        # 'READS_WITH_MATCHES_FASTA': { },
+        # 'READS_WITHOUT_MATCHES_FASTA': { },
+        # 'PREDICTED_CDS': { },
+        # 'PREDICTED_CDS_WITHOUT_ANNOTATION': { },
+        # 'PREDICTED_ORF_WITHOUT_ANNOTATION': { },
+
+        # taxonomy result files
+        # 'R_RNA_5S_FASTA': { },
+        # 'R_RNA_16S_FASTA': { },
+        # 'R_RNA_23S_FASTA': { },
+        'otu_table.tsv': {
+            'display': 'OTUTable',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table', 'real_ext': 'txt',
+            'suffix': 'otu_table', 'ext': 'tsv'
+        },
+        'otu_table_hdf5.biom': {
+            'display': 'OTUTableHDF5',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_hdf5', 'real_ext': 'biom',
+            'suffix': 'otu_table_hdf5', 'ext': 'biom'
+        },
+        'otu_table_json.biom': {
+            'display': 'OTUTableJSON',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_json', 'real_ext': 'biom',
+            'suffix': 'otu_table_json', 'ext': 'biom'
+        },
+        'pruned.tree': {
+            'display': 'PrunedTree',
+            'subdir': 'cr_otus',
+            'real_suffix': 'pruned', 'real_ext': 'tree',
+            'suffix': 'pruned', 'ext': 'tree'
+        },
+
         # functional result files
         'InterPro.tsv': {
             'display': 'InterProMatches',
@@ -36,6 +104,35 @@ DOWNLOAD_REF = {
         },
     },
     '3.0': {
+
+        # taxonomy result files
+        # 'R_RNA_5S_FASTA': { },
+        # 'R_RNA_16S_FASTA': { },
+        # 'R_RNA_23S_FASTA': { },
+        'otu_table.tsv': {
+            'display': 'OTUTable',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table', 'real_ext': 'txt',
+            'suffix': 'otu_table', 'ext': 'tsv'
+        },
+        'otu_table_hdf5.biom': {
+            'display': 'OTUTableHDF5',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_hdf5', 'real_ext': 'biom',
+            'suffix': 'otu_table_hdf5', 'ext': 'biom'
+        },
+        'otu_table_json.biom': {
+            'display': 'OTUTableJSON',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_json', 'real_ext': 'biom',
+            'suffix': 'otu_table_json', 'ext': 'biom'
+        },
+        'pruned.tree': {
+            'display': 'PrunedTree',
+            'subdir': 'cr_otus',
+            'real_suffix': 'pruned', 'real_ext': 'tree',
+            'suffix': 'pruned', 'ext': 'tree'
+        },
 
         # functional result files
         'InterPro.tsv': {
@@ -57,6 +154,35 @@ DOWNLOAD_REF = {
     },
     '4.0': {
 
+        # taxonomy result files
+        # 'R_RNA_5S_FASTA': { },
+        # 'R_RNA_16S_FASTA': { },
+        # 'R_RNA_23S_FASTA': { },
+        'otu_table.tsv': {
+            'display': 'OTUTable',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table', 'real_ext': 'txt',
+            'suffix': 'otu_table', 'ext': 'tsv'
+        },
+        'otu_table_hdf5.biom': {
+            'display': 'OTUTableHDF5',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_hdf5', 'real_ext': 'biom',
+            'suffix': 'otu_table_hdf5', 'ext': 'biom'
+        },
+        'otu_table_json.biom': {
+            'display': 'OTUTableJSON',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_json', 'real_ext': 'biom',
+            'suffix': 'otu_table_json', 'ext': 'biom'
+        },
+        'pruned.tree': {
+            'display': 'PrunedTree',
+            'subdir': 'cr_otus',
+            'real_suffix': 'pruned', 'real_ext': 'tree',
+            'suffix': 'pruned', 'ext': 'tree'
+        },
+
         # functional result files
         'InterPro.tsv': {
             'display': 'InterProMatches',
@@ -76,6 +202,35 @@ DOWNLOAD_REF = {
 
     },
     '4.1': {
+
+        # taxonomy result files
+        # 'R_RNA_5S_FASTA': { },
+        # 'R_RNA_16S_FASTA': { },
+        # 'R_RNA_23S_FASTA': { },
+        'otu_table.tsv': {
+            'display': 'OTUTable',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table', 'real_ext': 'txt',
+            'suffix': 'otu_table', 'ext': 'tsv'
+        },
+        'otu_table_hdf5.biom': {
+            'display': 'OTUTableHDF5',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_hdf5', 'real_ext': 'biom',
+            'suffix': 'otu_table_hdf5', 'ext': 'biom'
+        },
+        'otu_table_json.biom': {
+            'display': 'OTUTableJSON',
+            'subdir': 'cr_otus',
+            'real_suffix': 'otu_table_json', 'real_ext': 'biom',
+            'suffix': 'otu_table_json', 'ext': 'biom'
+        },
+        'pruned.tree': {
+            'display': 'PrunedTree',
+            'subdir': 'cr_otus',
+            'real_suffix': 'pruned', 'real_ext': 'tree',
+            'suffix': 'pruned', 'ext': 'tree'
+        },
 
         # functional result files
         'InterPro.tsv': {
