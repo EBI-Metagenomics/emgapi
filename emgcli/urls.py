c5602b9e4433034a3ef4f4c851fec58ff8c2a7b5
Miguel Boland <mdb@ebi.ac.uk>
2018-10-16 10:01:17 +0100

    Added custom auth form to override username field label

--- emgcli/urls.py
+++ emgcli/urls.py
@@ -38,6 +38,7 @@ from openapi_codec import OpenAPICodec
 
 from . import routers
 from .views import Handler500
+from emgui.forms import CustomAuthenticationForm
 
 
 handler500 = Handler500.as_error_view()
@@ -68,11 +69,12 @@ router.extend(mongo_router)
 router.extend(mydata_router)
 router.extend(utils_router)
 
+customLoginView = views.LoginView
+customLoginView.form_class = CustomAuthenticationForm
+
 urlpatterns = [
-    url(r'^http-auth/login_form$',
-        views.LoginView.as_view(
-            template_name='rest_framework/login_form.html'),
-        {}),
+    url(r'^http-auth/login_form$', customLoginView.as_view(
+        template_name='rest_framework/login_form.html'), {}),
 
     url(r'^http-auth/', include('rest_framework.urls',
                                 namespace='rest_framework')),
