e885fe8b5fc606d5b308c17a19a3f32015cc010c
mscheremetjew <maxim@ebi.ac.uk>
2019-07-05 15:05:54 +0100

    intemediate state.

--- emgapianns/management/commands/import_analysis.py
+++ emgapianns/management/commands/import_analysis.py
@@ -27,27 +27,38 @@ ena = ena_handler.EnaApiHandler()
 class Importer:
 
     def __init__(self, rootpath, accession):
-        self.rootpath = os.path.realpath(rootpath).strip()
-        self.accession = accession
+        """
+
+        :param rootpath: NFS root path of the results archive
+        :param accession: ENA accession for run or assembly/analysis
+        """
+        self._rootpath = os.path.realpath(rootpath).strip()
+        self._accession = accession
         self.existence_check()
+
+    def get_rootpath(self):
+        return self._rootpath
+
+    def get_accession(self):
+        return self._accession
 
     def existence_check(self):
         """
             Check if result folder does exit on the production file system.
         :return:
         """
-        if not os.path.exists(self.rootpath):
-            raise FileNotFoundError('Results dir {} does not exist'.format(self.rootpath))
+        if not os.path.exists(self._rootpath):
+            raise FileNotFoundError('Results dir {} does not exist'.format(self._rootpath))
 
-    def sanity_check_dir(self, library_strategy):
+    def sanity_check_dir(self, experiment_type):
         """
             Step 1: Search run/assembly result folder
             Step 2: Perform sanity check on result folder
-        :type library_strategy: Possible values are AMPLICON, WGS, RNA-Seq, ASSEMBLY
+        :type experiment_type: Possible values are AMPLICON, WGS, RNA-Seq, ASSEMBLY
         :return:
         """
         # Step 1
-        utils.retrieve_existing_result_dir(self.rootpath, self.accession)
+        utils.retrieve_existing_result_dir(self._rootpath, self.get_accession())
 
         pass
 
@@ -56,7 +67,7 @@ class Importer:
             Retrieves metadata from the ENA for the given run or assembly accession.
         :return:
         """
-        return ena.get_run(run_accession=self.accession)
+        return ena.get_run(run_accession=self.get_accession())
 
     def get_or_create_study(self, study_accession):
         pass
@@ -121,36 +132,42 @@ class Command(BaseCommand):
 
         logger.info("CLI %r" % options)
 
-        raw_accession = importer.get_accession()
+        is_assembly = utils.is_assembly(importer.get_accession())
+        logger.info("Identified assembly accession: {0}".format(is_assembly))
 
-        raw_metadata = importer.get_raw_metadata()
+        if is_assembly:
+            assembly, experiment_type = utils.parse_assembly_metadata(
+                ena.get_assembly(assembly_name=importer.get_accession()))
+            pass
+        else:  # Run accession detected
+            run, experiment_type = utils.parse_run_metadata(ena.get_run(run_accession=importer.get_accession()))
 
-        importer.sanity_check_dir()
+        importer.sanity_check_dir(experiment_type)
 
-        study_accession = raw_metadata['secondary_study_accession']
+        study = importer.get_or_create_study(study_accession)
+        #
+        # publication = self.get_or_create_publication()
+        # self.link_study_publication(study, publication)
+        #
+        # sample = self.get_or_create_sample(raw_metadata['sample_accession'])
+        # self.link_study_sample(study, sample)
+        #
+        # analysis = self.get_or_create_analysis(raw_metadata)
+        #
+        # if self.is_run(raw_metadata):
+        #     run = self.get_or_create_run(raw_metadata)
+        #     self.link_run_sample(run, sample)
+        #     self.link_analysis_run(analysis, run)
+        # elif self.is_assembly(raw_metadata):
+        #     assembly = self.get_or_create_assembly(raw_metadata)
+        #     self.link_assembly_sample(assembly, sample)
+        #     self.link_analysis_assembly(analysis, assembly)
+        # else:
+        #     raise ValueError(
+        #         'Could not determine if object is run or assembly from accession.')
+        #
+        # # TODO: If assembly is linked to run create RunAssembly object
+        #
+        # self.load_api_files(raw_accession, self.rootpath)
 
-        study = self.get_or_create_study(study_accession)
-
-        publication = self.get_or_create_publication()
-        self.link_study_publication(study, publication)
-
-        sample = self.get_or_create_sample(raw_metadata['sample_accession'])
-        self.link_study_sample(study, sample)
-
-        analysis = self.get_or_create_analysis(raw_metadata)
-
-        if self.is_run(raw_metadata):
-            run = self.get_or_create_run(raw_metadata)
-            self.link_run_sample(run, sample)
-            self.link_analysis_run(analysis, run)
-        elif self.is_assembly(raw_metadata):
-            assembly = self.get_or_create_assembly(raw_metadata)
-            self.link_assembly_sample(assembly, sample)
-            self.link_analysis_assembly(analysis, assembly)
-        else:
-            raise ValueError(
-                'Could not determine if object is run or assembly from accession.')
-
-        # TODO if assembly is linked to run create RunAssembly object
-
-        self.load_api_files(raw_accession, self.rootpath)
+        logger.info("Program finished successfully.")
