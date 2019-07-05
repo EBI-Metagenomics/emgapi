f4da323e80f1e42287312354e93206026d82b4ac
Miguel Boland <migueldboland@gmail.com>
2019-07-05 14:34:30 +0100

    Implemented and tested basic sanity check, extra config may be required for  AMPLICON/WGS/ASSEMBLY distinctions

--- emgapianns/management/lib/sanity_check.py
+++ emgapianns/management/lib/sanity_check.py
@@ -14,36 +14,105 @@ def get_dir_files(d):
 
 
 def check_content_match(expected_files, result_dir_files):
-    if expected_files != result_dir_files:
-        missing_files = get_missing_filelist(expected_files,
-                                             result_dir_files)
+    missing_files = get_missing_filelist(expected_files, result_dir_files)
+    if missing_files:
         raise AssertionError('{} are missing'.format(missing_files))
 
 
 class BasicSanityCheck:
-    result_files = [
-    ]
+    result_files = {
+        '{}_CDS_annotated.faa.chunks',
+        '{}_CDS_annotated.faa.gz',
+        '{}_CDS_annotated.ffn.chunks',
+        '{}_CDS_annotated.ffn.gz',
+        '{}_CDS_unannotated.faa.chunks',
+        '{}_CDS_unannotated.faa.gz',
+        '{}_CDS_unannotated.ffn.chunks',
+        '{}_CDS_unannotated.ffn.gz',
+        '{}.cmsearch.all.tblout.deoverlapped.gz',
+        '{}.cmsearch.all.tblout.gz',
+        '{}.fasta.chunks',
+        '{}.fasta.gz',
+        '{}.fasta.submitted.count',
+        '{}_I5.tsv.chunks',
+        '{}_I5.tsv.gz',
+        '{}_run.err',
+        '{}_run.log',
+        '{}_summary',
+        '{}_summary.go',
+        '{}_summary.go_slim',
+        '{}_summary.ipr',
+        '{}_summary.rfam',
+        'globalsummary.out',
+        'go-summary.out',
+        'README',
+        'RESOURCE_USAGE_PROFILE'
+    }
 
-    taxonomy_subdirs = {'SSU', 'LSU'}
+    taxonomy_subdirs = {
+        'SSU', 'LSU'
+    }
+
     taxonomy_result_files = {
         'krona.html',
         'kingdom-counts.txt',
         '{}.fasta.mseq.tsv',
+        '{}.fasta.mseq.txt',
         '{}.fasta.mseq.gz',
         '{}.fasta.mseq_hdf5.biom',
         '{}.fasta.mseq_json.biom',
     }
-    flag_files = [
-        ''
-    ]
 
-    extra_files = []
+    qc_files = {
+        'GC-distribution.out.full',
+        'GC-distribution.out.full_bin',
+        'GC-distribution.out.full_pcbin',
+        'nucleotide-distribution.out.full',
+        'seq-length.out.full',
+        'seq-length.out.full_bin',
+        'seq-length.out.full_pcbin',
+        'summary.out'
+    }
+    flag_files = {
+        'stepCDS-success',
+        'stepChunkingAndCompression-success',
+        'stepCMSearchDeoverlap-success',
+        'stepCMSearch-success',
+        'stepDecompression-success',
+        'stepFastaHeaderClean-success',
+        'stepFinalCleaning-success',
+        'stepGOSummary-success',
+        'stepI5tsv-success',
+        'stepInit-success',
+        'stepMAPseq-success',
+        'stepNewCharts-success',
+        'stepQCStats-success',
+        'stepRNASelection-success',
+        'stepRNASummary-success',
+        'stepSequenceCategorisation-success',
+        'stepSummary-success',
+        'stepTaxonomySummary-success',
+        'sync-success'
+    }
+
+    seq_categorisation = {
+        'interproscan.fasta.chunks',
+        'interproscan.fasta.gz',
+        'LSU.fasta.chunks',
+        'LSU.fasta.gz',
+        'noFunction.fasta.chunks',
+        'noFunction.fasta.gz',
+        'pCDS.fasta.chunks',
+        'pCDS.fasta.gz',
+        'SSU.fasta.chunks',
+        'SSU.fasta.gz'
+    }
 
     def __init__(self, d, prefix):
         self.dir = d
         self.prefix = prefix
         if hasattr(self, 'extra_files'):
-            self.result_files.extend(getattr(self, 'extra_files'))
+            self.result_files.union(getattr(self, 'extra_files'))
 
         assert len(self.result_files) > 0
         assert len(self.flag_files) > 0
@@ -52,10 +121,11 @@ class BasicSanityCheck:
         prefix = prefix or self.prefix
         return {f.format(prefix) for f in filelist}
 
+    def check_flags(self):
+        self.check_dir_content(self.dir, self.flag_files)
+
     def check_maindir_results(self):
-        expected_files = self.gen_result_files(self.result_files)
-        result_dir_files = get_dir_files(self.dir)
-        check_content_match(expected_files, result_dir_files)
+        self.check_dir_content(self.dir, self.result_files)
 
     def check_taxonomy_results(self):
         tax_dir = os.path.join(self.dir, 'taxonomy-summary')
@@ -63,32 +133,44 @@ class BasicSanityCheck:
             raise AssertionError('taxonomy-summary dir is missing')
 
         subunit_dirs = os.listdir(tax_dir)
-        for su_type in subunit_dirs:
-            subdir = os.path.join(tax_dir, su_type)
-            self.check_taxonomy(subdir, su_type)
-        else:
+
+        if len(subunit_dirs) == 0:
             raise AssertionError('No SSU/LSU dir found in taxonomy-summary')
 
-    def check_taxonomy(self, subdir, su_type):
-        prefix = self.prefix + '_' + su_type
-        expected_files = self.gen_result_files(self.taxonomy_result_files, prefix)
-        tax_dir_content = get_dir_files(subdir)
-        check_content_match(expected_files, tax_dir_content)
+        for su_type in subunit_dirs:
+            subdir = os.path.join(tax_dir, su_type)
+            prefix = self.prefix + '_' + su_type
+            self.check_dir_content(subdir, self.taxonomy_result_files, prefix=prefix)
 
-    def check(self):
+    def check_qc_results(self):
+        dirpath = os.path.join(self.dir, 'qc-statistics')
+        self.check_dir_content(dirpath, self.qc_files)
+
+    def check_seq_categorisation(self):
+        dirpath = os.path.join(self.dir, 'sequence-categorisation')
+        self.check_dir_content(dirpath, self.seq_categorisation)
+
+    def check_dir_content(self, dirpath, content, prefix=None):
+        expected_files = self.gen_result_files(content, prefix)
+        if not os.path.exists(dirpath):
+            raise AssertionError('Missing directory {}'.format(dirpath))
+        result_dir_files = get_dir_files(dirpath)
+        check_content_match(expected_files, result_dir_files)
+
+    def check_all(self):
+        # self.check_flags()
         self.check_maindir_results()
         self.check_taxonomy_results()
+        self.check_qc_results()
+        self.check_seq_categorisation()
 
 
 class AmpliconSanityCheck(BasicSanityCheck):
-    taxonomy_subdirs = {'SSU'}
-    extra_files = [
-        '{}.fasta.chunks',
-    ]
+    extra_files = {}
 
 
 class WgsSanityCheck(BasicSanityCheck):
-    extra_files = []
+    extra_files = {}
 
 
 def run_sanity_check(d, job_prefix, lib_strategy):
@@ -99,4 +181,4 @@ def run_sanity_check(d, job_prefix, lib_strategy):
         'OTHER': WgsSanityCheck
     }
     cls = lib_cls[lib_strategy]
-    return cls(d, job_prefix).check()
+    return cls(d, job_prefix).check_all()
--- tests/unit/test_sanity_check.py
+++ tests/unit/test_sanity_check.py
@@ -11,16 +11,60 @@ def write_file(path, content):
     path.write_text(content, encoding='UTF-8')
 
 
+def gen_all_tax_files(su_types, tmpdir, prefix):
+    tax_dir = tmpdir / 'taxonomy-summary'
+    tax_dir.mkdir()
+
+    for su_type in su_types:
+        path = tax_dir / su_type
+        path.mkdir()
+        pref = prefix + '_' + su_type
+        filelist = sanity_check.AmpliconSanityCheck.taxonomy_result_files
+        files = [f.format(pref) for f in filelist]
+        for f in files:
+            write_file(path / f, 'data')
+
+
+def gen_all_flag_files(tmpdir):
+    filelist = sanity_check.AmpliconSanityCheck.flag_files
+    for f in filelist:
+        write_file(tmpdir / f, 'data')
+
+
+def gen_all_qc_files(tmpdir):
+    qc_dir = tmpdir / 'qc-statistics'
+    qc_dir.mkdir()
+
+    filelist = sanity_check.AmpliconSanityCheck.qc_files
+    for f in filelist:
+        write_file(qc_dir / f, 'data')
+
+
+def gen_all_maindir_files(tmpdir, prefix):
+    filelist = sanity_check.AmpliconSanityCheck.result_files
+    files = [f.format(prefix) for f in filelist]
+    for f in files:
+        write_file(tmpdir / f, 'data')
+    write_file(tmpdir / prefix + '.fasta.chunks', 'mydata')
+
+
+def gen_all_seqdir_files(tmpdir):
+    seq_dir = tmpdir / 'sequence-categorisation'
+    seq_dir.mkdir()
+    filelist = sanity_check.AmpliconSanityCheck.seq_categorisation
+    for f in filelist:
+        write_file(seq_dir / f, 'data')
+
+
 class TestSanityCheck:
     def test_amplicon_maindir_results_should_not_raise_exception(self, tmpdir):
         prefix = 'SRR6418294_MERGED_FASTQ'
-        write_file(tmpdir / prefix + '.fasta.chunks', 'mydata')
+        gen_all_maindir_files(tmpdir, prefix)
         tmpdir = str(tmpdir)
         cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
         cls.check_maindir_results()
 
-    def test_amplicon_maindir_results_should_fail_on_missing_file(self,
-                                                                  tmpdir):
+    def test_amplicon_maindir_results_should_raise_error_on_missing_file(self, tmpdir):
         prefix = 'SRR6418294_MERGED_FASTQ'
         tmpdir = str(tmpdir)
         cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
@@ -32,29 +76,103 @@ class TestSanityCheck:
         ('SSU',),
         ('LSU',)
     ])
-    def test_amplicon_taxonomy_results_single_subunit(self, su_types, tmpdir):
+    def test_amplicon_taxonomy_results_varying_subunit(self, su_types, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        gen_all_tax_files(su_types, tmpdir, prefix)
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        cls.check_taxonomy_results()
+
+    def test_amplicon_taxonomy_results_missing_subunits(self, tmpdir):
         prefix = 'SRR6418294_MERGED_FASTQ'
         tax_dir = tmpdir / 'taxonomy-summary'
         tax_dir.mkdir()
 
-        for su_type in su_types:
-            path = tax_dir / su_type
-            path.mkdir()
-            pref = prefix + '_' + su_type
-            filelist = sanity_check.AmpliconSanityCheck.taxonomy_result_files
-            files = [f.format(pref) for f in filelist]
-            for f in files:
-                write_file(path / f, 'data')
-
         tmpdir = str(tmpdir)
         cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
         with pytest.raises(AssertionError):
             cls.check_taxonomy_results()
 
-    def test_amplicon_taxonomy_results_should_fail_on_missing_file(self,
-                                                                   tmpdir):
+    def test_amplicon_taxonomy_results_should_raise_error_on_missing_tax_dir(self, tmpdir):
         prefix = 'SRR6418294_MERGED_FASTQ'
         tmpdir = str(tmpdir)
         cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
         with pytest.raises(AssertionError):
             cls.check_taxonomy_results()
+
+    def test_check_qc_should_raise_error_on_missing_dir(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        with pytest.raises(AssertionError):
+            cls.check_qc_results()
+
+    def test_qc(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        gen_all_qc_files(tmpdir)
+
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        cls.check_qc_results()
+
+    def test_check_flags(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        gen_all_flag_files(tmpdir)
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        cls.check_flags()
+
+    def test_check_flags_should_raise_error_on_missing_flags(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        with pytest.raises(AssertionError):
+            cls.check_flags()
+
+    def test_check_seqdir(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        gen_all_seqdir_files(tmpdir)
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        cls.check_seq_categorisation()
+
+    def test_check_seqdir_should_raise_error_on_missing_flags(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        with pytest.raises(AssertionError):
+            cls.check_seq_categorisation()
+
+    def test_check_all_should_raise_error_on_missing_files(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        with pytest.raises(AssertionError):
+            cls.check_all()
+
+    def test_check_all_should_work_on_valid_dir(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        gen_all_seqdir_files(tmpdir)
+        gen_all_maindir_files(tmpdir, prefix)
+        gen_all_qc_files(tmpdir)
+        gen_all_tax_files(['SSU', 'LSU'], tmpdir, prefix)
+        gen_all_flag_files(tmpdir)
+        tmpdir = str(tmpdir)
+        cls = sanity_check.AmpliconSanityCheck(tmpdir, prefix)
+        cls.check_all()
+
+    def test_run_sanity_check_should_raise_error_on_missing_files(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        tmpdir = str(tmpdir)
+        with pytest.raises(AssertionError):
+            sanity_check.run_sanity_check(tmpdir, prefix, 'AMPLICON')
+
+    def test_run_sanity_check_should_pass(self, tmpdir):
+        prefix = 'SRR6418294_MERGED_FASTQ'
+        gen_all_seqdir_files(tmpdir)
+        gen_all_maindir_files(tmpdir, prefix)
+        gen_all_qc_files(tmpdir)
+        gen_all_tax_files(['SSU', 'LSU'], tmpdir, prefix)
+        gen_all_flag_files(tmpdir)
+        tmpdir = str(tmpdir)
+        sanity_check.run_sanity_check(tmpdir, prefix, 'AMPLICON')
