cf73465e0eed58bbe9d37d2d5784dec7b37639ca
mscheremetjew <maxim@ebi.ac.uk>
2019-07-05 14:56:22 +0100

    Introduced a few model classes which could easily expanded.

---
+++ emgapianns/management/commands/import_analysis_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 EMBL - European Bioinformatics Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from enum import Enum
+
+
+class Run(object):
+
+    def __init__(self, study_accession, sample_accession, run_accession, experiment_type):
+        self.study_accession = study_accession
+        self.sample_accession = sample_accession
+        self.run_accession = run_accession
+        self.experiment_type = experiment_type
+
+
+class Assembly(object):
+
+    def __init__(self, study_accession, sample_accession, run_accession, analysis_accession):
+        self.study_accession = study_accession
+        self.sample_accession = sample_accession
+        self.run_accession = run_accession
+        self.analysis_accession = analysis_accession
+
+
+class ExperimentType(Enum):
+    AMPLICON = 'AMPLICON'
+    WGS = 'WGS'
+    OTHER = 'OTHER'
+    RNA_SEQ = 'RNA-Seq'
+    ASSEMBLY = 'ASSEMBLY'
