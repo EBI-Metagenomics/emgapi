4518cda4c06ce9082f9ba7114ff52052b324dc0f
Miguel Boland <migueldboland@gmail.com>
2019-07-18 11:16:48 +0100

    Finished implementing and testing import_sample

--- tests/unit/test_import_samples.py
+++ tests/unit/test_import_samples.py
@@ -69,7 +69,7 @@ class TestImportSampleTransactions(TransactionTestCase):
 
     @pytest.mark.usefixtures("biome")
     @pytest.mark.usefixtures("var_names")
-    def test_import_sample_should_not_load_location(self):
+    def test_import_sample_should_not_load_location_if_empty_api_field(self):
         sample_accession = 'ERS1282031'
         mock_api_data = mock_fetch_sample_api()
         del mock_api_data['location']
@@ -95,3 +95,77 @@ class TestImportSampleTransactions(TransactionTestCase):
             created_sample = emg_models.Sample.objects.get(accession=sample_accession)
             assert created_sample.latitude == 44.0
             assert created_sample.longitude == 53.0
+
+    @pytest.mark.usefixtures("biome")
+    @pytest.mark.usefixtures("var_names")
+    def test_import_sample_should_load_var_names(self):
+        sample_accession = 'ERS1282031'
+        mock_api_data = mock_fetch_sample_api()
+        mock_api = mock.patch.object(Command, 'fetch_sample_api', new=lambda *args, **kwargs: mock_api_data)
+        mock_db = mock.patch.object(Command, 'fetch_sample_ena_db', new=create_model)
+        with mock_api, mock_db:
+            cmd = Command()
+            cmd.run_from_argv(argv=['manage.py', 'import_sample', sample_accession, '--biome', 'root:foo:bar'])
+            created_sample = emg_models.Sample.objects.get(accession=sample_accession)
+            annotations = emg_models.SampleAnn.objects.filter(sample=created_sample)
+            assert annotations.get(var__var_name='geographic location (latitude)').var_val_ucv == '44.0'
+            assert annotations.get(var__var_name='geographic location (longitude)').var_val_ucv == '53.0'
+            assert annotations.get(var__var_name='collection date').var_val_ucv == '2019-01-01'
+
+    @pytest.mark.usefixtures("biome")
+    @pytest.mark.usefixtures("var_names")
+    def test_import_sample_should_tag_to_study(self):
+        sample_accession = 'ERS1282031'
+        fake_study = 'ERP001736'
+        mock_api_data = mock_fetch_sample_api()
+        mock_api = mock.patch.object(Command, 'fetch_sample_api', new=lambda *args, **kwargs: mock_api_data)
+        mock_db = mock.patch.object(Command, 'fetch_sample_ena_db', new=create_model)
+
+        s = emg_models.Study(secondary_accession=fake_study,
+                             is_public=True,
+                             last_update='2019-01-01',
+                             first_created='2019-01-01',
+                             biome=emg_models.Biome.objects.get(lineage='root:foo:bar'))
+        s.save()
+        mock_api_studies = mock.patch.object(Command, 'get_sample_studies', new=lambda *args, **kwargs: {fake_study})
+        with mock_api, mock_db, mock_api_studies:
+            cmd = Command()
+            cmd.run_from_argv(argv=['manage.py', 'import_sample', sample_accession, '--biome', 'root:foo:bar'])
+            created_sample = emg_models.Sample.objects.get(accession=sample_accession)
+            assert emg_models.StudySample.objects.get(study=s, sample=created_sample)
+
+    @pytest.mark.usefixtures("biome")
+    @pytest.mark.usefixtures("var_names")
+    def test_import_sample_duplicate_import_should_not_duplicate_objects(self):
+        sample_accession = 'ERS1282031'
+        fake_study = 'ERP001736'
+        mock_api_data = mock_fetch_sample_api()
+        mock_api = mock.patch.object(Command, 'fetch_sample_api', new=lambda *args, **kwargs: mock_api_data)
+        mock_db = mock.patch.object(Command, 'fetch_sample_ena_db', new=create_model)
+
+        s = emg_models.Study(secondary_accession=fake_study,
+                             is_public=True,
+                             last_update='2019-01-01',
+                             first_created='2019-01-01',
+                             biome=emg_models.Biome.objects.get(lineage='root:foo:bar'))
+        s.save()
+        mock_api_studies = mock.patch.object(Command, 'get_sample_studies', new=lambda *args, **kwargs: {fake_study})
+        with mock_api, mock_db, mock_api_studies:
+            cmd = Command()
+            cmd.run_from_argv(argv=['manage.py', 'import_sample', sample_accession, '--biome', 'root:foo:bar'])
+            cmd.run_from_argv(argv=['manage.py', 'import_sample', sample_accession, '--biome', 'root:foo:bar'])
+            created_sample = emg_models.Sample.objects.get(accession=sample_accession)
+            assert len(emg_models.StudySample.objects.filter(study=s, sample=created_sample)) == 1
+            assert len(emg_models.Sample.objects.filter(accession=sample_accession)) == 1
+            assert len(emg_models.SampleAnn.objects.filter(sample=created_sample)) == 3
+
+    @pytest.mark.usefixtures("biome")
+    def test_import_should_fail_if_varnames_missing(self):
+        sample_accession = 'ERS1282031'
+        mock_api_data = mock_fetch_sample_api()
+        mock_api = mock.patch.object(Command, 'fetch_sample_api', new=lambda *args, **kwargs: mock_api_data)
+        mock_db = mock.patch.object(Command, 'fetch_sample_ena_db', new=create_model)
+        with mock_api, mock_db:
+            cmd = Command()
+            with pytest.raises(emg_models.VariableNames.DoesNotExist):
+                cmd.run_from_argv(argv=['manage.py', 'import_sample', sample_accession, '--biome', 'root:foo:bar'])
