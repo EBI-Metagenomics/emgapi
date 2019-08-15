d2e49c709efa86cfac5d016e1c29b1c505f4a03c
Martin Beracochea <mbc@ebi.ac.uk>
2019-08-15 14:51:01 +0100

    Fix the csv renderer and small perf on samples endpoint.
    Paginate the CSV results for the CSV renderer, and fix for single elements.
    
    Added a simple paginator on the samples endpoint that is faster because
    it just returns the pk for the count (useful for complex queries)

--- emgcli/pagination.py
+++ emgcli/pagination.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright 2017 EMBL - European Bioinformatics Institute
+# Copyright 2019 EMBL - European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,32 @@
 # limitations under the License.
 
 from rest_framework_json_api import pagination
+from django.core.paginator import Paginator
+from django.utils.functional import cached_property
 
 
 class DefaultPagination(pagination.PageNumberPagination):
 
     page_size = 25
     max_page_size = 250
+
+
+class FasterDjangoPaginator(Paginator):
+    """
+    DjangoPaginator override.
+    This class overrides the count method, selecting only the PK
+    this makes the count faster as it doesn't need to fetch all
+    the cols just to do a count.
+    """
+
+    @cached_property
+    def count(self):
+        # Only get the pk to make the pagination faster
+        return self.object_list.values('pk').count()
+
+
+class FasterCountPagination(pagination.PageNumberPagination):
+
+    page_size = 25
+    max_page_size = 250
+    django_paginator_class = FasterDjangoPaginator
