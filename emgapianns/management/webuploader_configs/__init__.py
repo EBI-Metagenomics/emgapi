99d903e53d85a4bfce76d61eabad125eaeaeda0d
mscheremetjew <maxim@ebi.ac.uk>
2019-09-30 20:05:57 +0100

    Applied experiment type fix.

--- emgapianns/management/webuploader_configs/__init__.py
+++ emgapianns/management/webuploader_configs/__init__.py
@@ -13,7 +13,7 @@ def get_downloadset_config(version, experiment_type):
     version_dir_name = 'v' + str(version).replace('.', '_')
     if experiment_type in experiment_type_remapping:
         experiment_type = experiment_type_remapping[experiment_type]
-    config_file = os.path.join(DIR_NAME, version_dir_name, experiment_type.value.lower() + '.json')
+    config_file = os.path.join(DIR_NAME, version_dir_name, experiment_type.lower() + '.json')
     return read_config(config_file)
 
 
