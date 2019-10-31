eafffd3363b10c1df29acb1bc7b39bd38a625178
Martín Beracochea <mbc@ebi.ac.uk>
2019-10-31 13:59:18 +0000

    Pipeline v5 antiSMASH (#106)
    V5 antiSMASH contigs endpoints

--- emgapi/utils.py
+++ emgapi/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018 EMBL - European Bioinformatics Institute
+# Copyright 2019 EMBL - European Bioinformatics Institute
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -68,20 +68,19 @@ def analysisjob_accession_query(accession):
 
 def assembly_contig_name(line):
     """Parses Mgnify (ENA) assembly contigs names and returns the contig name with no metadata.
-    For example:
-    ENA-OKXA01000001-OKXA01000001.1-human-gut-metagenome-strain-
-    SKBSTL060-genome-assembly--contig:-NODE-1-length-34650-cov-6.786732
-    get the the contig name: NODE-1-length-34650-cov-6.786732
-
+    Example:
+        ENA-OKXA01000001-OKXA01000001.1-human-gut-metagenome-strain-
+        SKBSTL060-genome-assembly--contig:-NODE-1-length-34650-cov-6.786732
+    will return the contig name: NODE-1-length-34650-cov-6.786732
     """
     return re.sub(r'[^\t=]*\-\-contig\:\-', '', line)
 
 
 def assembly_contig_coverage(name):
     """From a contig name return the coverage (from the fasta name)
-
     Example:
-    From NODE-1-length-34650-cov-6.786732 will return 6.786732 or '-' if failed.
+        NODE-1-length-34650-cov-6.786732
+    will return 6.786732 or 0 if failed.
     """
     match = re.match(r'.*cov[-|_](?P<cov>\d*\.?\d*)', name)
     if match:
