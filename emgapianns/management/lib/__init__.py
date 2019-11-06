dbc7ff613a440af699cb764b7c9c431e70f8584b
Martin Beracochea <mbc@ebi.ac.uk>
2019-11-06 13:55:25 +0000

    Support older version of the Pipeline on import commands

--- emgapianns/management/lib/__init__.py
+++ emgapianns/management/lib/__init__.py
@@ -35,7 +35,14 @@ class EMGBaseCommand(BaseCommand):
             help='Pipeline version',
             action='store',
             dest='pipeline',
-            choices=['4.0', '4.1', '5.0'], default='4.1'
+            choices=[
+                '1.0',
+                '2.0',
+                '3.0',
+                '4.0',
+                '4.1',
+                '5.0'],
+            default='4.1'
         )
 
     def handle(self, *args, **options):
