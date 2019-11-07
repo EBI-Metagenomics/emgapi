b5249c0bbee4a93ed79569bc74bedd463d02efd6
mscheremetjew <maxim@ebi.ac.uk>
2019-11-07 11:17:52 +0000

    Added 2 more test cases to cover NCBI studies.

--- tests/functional/test_import_study.py
+++ tests/functional/test_import_study.py
@@ -30,6 +30,60 @@ def mock_ena_run_study(*args, **kwargs):
     return study
 
 
+def mock_ncbi_run_study_SRP000125():
+    """
+        Useful tests for this case:
+            List of pubmed_ids
+    :return:
+    """
+    study = ena_models.RunStudy()
+    study.study_id = 'SRP000125'
+    study.project_id = 'PRJNA17765'
+    study.study_status = 'public'
+    study.center_name = 'SDSU Center for Universal Microbial Sequencing'
+    study.hold_date = None
+    study.first_created = '2010-02-26'
+    study.last_updated = '2017-09-25'
+    study.study_title = 'Marine phages from the Gulf of Mexico'
+    study.study_description = 'A combined set of 41 samples was isolated by the Suttle Laboratory from 13 ' \
+                              'different sites in the Gulf of Mexico between June 1, 1994 and July 31, 2001.  ' \
+                              'The phage fraction was purified and sequenced using pyrophosphate sequencing ' \
+                              '(454 Life Sciences).    This is part of a global ocean survey of phage and ' \
+                              'virus sequences.   454 sequence data is available from the Short Read Archive ' \
+                              '(SRA): <a href="ftp://ftp.ncbi.nih.gov/pub/TraceDB/ShortRead/SRA000408  ' \
+                              '">SRA000408</a>.   Metagenomics SEED ID: 4440304.3  Nature paper ID: 32  ' \
+                              'The WGS project can be found using the Project data link.'
+    study.submission_account_id = 'Webin-842'
+    study.pubmed_id = '17090214,18337718'
+    return study
+
+
+def mock_ncbi_run_study_SRP034734():
+    """
+        Useful tests for this case:
+            pubmed_id = None
+            Center name for BioProjects
+
+    :return:
+    """
+    study = ena_models.RunStudy()
+    study.study_id = 'SRP034734'
+    study.project_id = 'PRJNA218849'
+    study.study_status = 'public'
+    study.center_name = 'Veer Narmad South Gujarat University'
+    study.hold_date = None
+    study.first_created = '2014-01-09'
+    study.last_updated = '2017-07-10'
+    study.study_title = 'Lonar Lake sediment Metagenome'
+    study.study_description = 'Understanding the relevance of bacterial and archaeal diversity in the ' \
+                              'soda lake sediments by the culture independent approach using bTEFAP.  ' \
+                              'Lonar Lake is a saline soda lake located at Lonar in Buldana district, ' \
+                              'Maharashtra State, India, which was created by a meteor impact.'
+    study.submission_account_id = 'Webin-842'
+    study.pubmed_id = None
+    return study
+
+
 def insert_biome(biome_id, biome_name, left, right, depth, lineage):
     emg_models.Biome.objects.update_or_create(biome_id=biome_id,
                                               biome_name=biome_name,
@@ -42,14 +96,14 @@ def insert_biome(biome_id, biome_name, left, right, depth, lineage):
 @pytest.mark.django_db
 class TestImportStudyTransactions(TransactionTestCase):
 
-    @pytest.mark.usefixtures("biome")
-    @pytest.mark.usefixtures("var_names")
+    # @pytest.mark.usefixtures("biome")
+    # @pytest.mark.usefixtures("var_names")
     # @pytest.mark.parametrize("accession, lineage", [
     #     ('ERP117125', 'root:Host-associated:Mammals:Digestive system:Fecal')
     # ])
     @patch('emgapianns.management.commands.import_study.Command.get_study_dir')
     @patch('emgapianns.management.lib.create_or_update_study.StudyImporter._fetch_study_metadata')
-    def test_import_study_should_succeed(self, mock_db, mock_study_dir):
+    def test_import_ena_study_should_succeed(self, mock_db, mock_study_dir):
         """
         :param mock_db:
         :param mock_study_dir:
@@ -60,7 +114,7 @@ class TestImportStudyTransactions(TransactionTestCase):
         biome_name = 'Fecal'
         #
         mock_study_dir.return_value = "2019/09/ERP117125"
-        mock_db.return_value = mock_ena_run_study()
+        mock_db.return_value = expected = mock_ena_run_study()
         #
         insert_biome(422, biome_name, 841, 844, 5, lineage)
 
@@ -69,23 +123,106 @@ class TestImportStudyTransactions(TransactionTestCase):
             cmd.run_from_argv(
                 argv=['manage.py', 'import_study', accession, lineage])
             actual_study = emg_models.Study.objects.get(secondary_accession=accession)
-            expected = mock_ena_run_study()
-            assert actual_study.secondary_accession == expected.study_id
-            assert actual_study.project_id == expected.project_id
-            assert actual_study.centre_name == expected.center_name
-            assert actual_study.experimental_factor == None
-            assert actual_study.is_public == True
-            assert actual_study.public_release_date == None
-            assert actual_study.study_abstract == expected.study_description
-            assert actual_study.study_name == expected.study_title
-            assert actual_study.study_status == 'FINISHED'
-            assert actual_study.data_origination == 'SUBMITTED'
-            assert actual_study.author_email == None
-            assert actual_study.author_name == None
-            assert actual_study.last_update.strftime("%m/%d/%Y") == timezone.now().strftime("%m/%d/%Y")
-            assert actual_study.submission_account_id == expected.submission_account_id
-            assert actual_study.biome.biome_name == biome_name
-            assert actual_study.result_directory == mock_study_dir.return_value
-            assert actual_study.first_created.strftime("%Y-%m-%d %H:%M:%S") == expected.first_created
-            assert len(actual_study.publications.all()) == 0
-            assert len(actual_study.samples.all()) == 0
+            assert expected.study_id == actual_study.secondary_accession
+            assert expected.project_id == actual_study.project_id
+            assert expected.center_name == actual_study.centre_name
+            assert None == actual_study.experimental_factor
+            assert True == actual_study.is_public
+            assert None == actual_study.public_release_date
+            assert expected.study_description == actual_study.study_abstract
+            assert expected.study_title == actual_study.study_name
+            assert 'FINISHED' == actual_study.study_status
+            assert 'SUBMITTED' == actual_study.data_origination
+            assert None == actual_study.author_email
+            assert None == actual_study.author_name
+            assert timezone.now().strftime("%m/%d/%Y") == actual_study.last_update.strftime("%m/%d/%Y")
+            assert expected.submission_account_id == actual_study.submission_account_id
+            assert biome_name == actual_study.biome.biome_name
+            assert mock_study_dir.return_value == actual_study.result_directory
+            assert expected.first_created == actual_study.first_created.strftime("%Y-%m-%d %H:%M:%S")
+            assert 0 == len(actual_study.publications.all())
+            assert 0 == len(actual_study.samples.all())
+
+    @patch('emgapianns.management.commands.import_study.Command.get_study_dir')
+    @patch('emgapianns.management.lib.create_or_update_study.StudyImporter._fetch_study_metadata')
+    def test_import_ncbi_study_SRP000125_should_succeed(self, mock_db, mock_study_dir):
+        """
+        :param mock_db:
+        :param mock_study_dir:
+        :return:
+        """
+        accession = 'SRP000125'
+        lineage = 'root:Host-associated:Mammals:Digestive system:Fecal'
+        biome_name = 'Fecal'
+        #
+        mock_study_dir.return_value = "2019/09/{}".format(accession)
+        mock_db.return_value = expected = mock_ncbi_run_study_SRP000125()
+        #
+        insert_biome(422, biome_name, 841, 844, 5, lineage)
+
+        with mock_db, mock_study_dir:
+            cmd = Command()
+            cmd.run_from_argv(
+                argv=['manage.py', 'import_study', accession, lineage])
+            actual_study = emg_models.Study.objects.get(secondary_accession=accession)
+            assert expected.study_id == actual_study.secondary_accession
+            assert expected.project_id == actual_study.project_id
+            assert expected.center_name == actual_study.centre_name
+            assert None == actual_study.experimental_factor
+            assert True == actual_study.is_public
+            assert None == actual_study.public_release_date
+            assert expected.study_description == actual_study.study_abstract
+            assert expected.study_title == actual_study.study_name
+            assert 'FINISHED' == actual_study.study_status
+            assert 'HARVESTED' == actual_study.data_origination
+            assert None == actual_study.author_email
+            assert None == actual_study.author_name
+            assert timezone.now().strftime("%m/%d/%Y") == actual_study.last_update.strftime("%m/%d/%Y")
+            assert expected.submission_account_id == actual_study.submission_account_id
+            assert biome_name == actual_study.biome.biome_name
+            assert mock_study_dir.return_value == actual_study.result_directory
+            assert expected.first_created == actual_study.first_created.strftime("%Y-%m-%d")
+            assert 2 == len(actual_study.publications.all())
+            assert 0 == len(actual_study.samples.all())
+
+    @patch('emgapianns.management.commands.import_study.Command.get_study_dir')
+    @patch('emgapianns.management.lib.create_or_update_study.StudyImporter._fetch_study_metadata')
+    def test_import_ncbi_study_SRP034734_should_succeed(self, mock_db, mock_study_dir):
+        """
+        :param mock_db:
+        :param mock_study_dir:
+        :return:
+        """
+        accession = 'SRP034734'
+        lineage = 'root:Host-associated:Mammals:Digestive system:Fecal'
+        biome_name = 'Fecal'
+        #
+        mock_study_dir.return_value = "2019/09/{}".format(accession)
+        mock_db.return_value = expected = mock_ncbi_run_study_SRP034734()
+        #
+        insert_biome(422, biome_name, 841, 844, 5, lineage)
+
+        with mock_db, mock_study_dir:
+            cmd = Command()
+            cmd.run_from_argv(
+                argv=['manage.py', 'import_study', accession, lineage])
+            actual_study = emg_models.Study.objects.get(secondary_accession=accession)
+            assert expected.study_id == actual_study.secondary_accession
+            assert expected.project_id == actual_study.project_id
+            assert expected.center_name == actual_study.centre_name
+            assert None == actual_study.experimental_factor
+            assert True == actual_study.is_public
+            assert None == actual_study.public_release_date
+            assert expected.study_description == actual_study.study_abstract
+            assert expected.study_title == actual_study.study_name
+            assert 'FINISHED' == actual_study.study_status
+            assert 'HARVESTED' == actual_study.data_origination
+            assert None == actual_study.author_email
+            assert None == actual_study.author_name
+            assert timezone.now().strftime("%m/%d/%Y") == actual_study.last_update.strftime("%m/%d/%Y")
+            assert expected.submission_account_id == actual_study.submission_account_id
+            assert biome_name == actual_study.biome.biome_name
+            assert mock_study_dir.return_value == actual_study.result_directory
+            assert expected.first_created == actual_study.first_created.strftime("%Y-%m-%d")
+            assert 0 == len(actual_study.publications.all())
+            assert 0 == len(actual_study.samples.all())
