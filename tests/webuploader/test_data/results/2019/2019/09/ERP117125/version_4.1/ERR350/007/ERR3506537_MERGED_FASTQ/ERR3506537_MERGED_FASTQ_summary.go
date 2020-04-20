4c7cc5a08891c587c7d7ba734757cf605bcb7e28
Maxim <maxim.scheremetjew@gmail.com>
2020-04-20 15:14:19 +0100

    Redundant test files removed.

--- tests/webuploader/test_data/results/2019/2019/09/ERP117125/version_4.1/ERR350/007/ERR3506537_MERGED_FASTQ/ERR3506537_MERGED_FASTQ_summary.go
+++
@@ -1,3 +0,0 @@
-"GO:0046718","viral entry into host cell","biological_process","2"
-"GO:0019028","viral capsid","cellular_component","4"
-"GO:0005198","structural molecule activity","molecular_function","2"
