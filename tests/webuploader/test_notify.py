d674c27e07b0029ddd09bf9252f32810652ac9c6
Martin Beracochea <mbc@ebi.ac.uk>
2020-12-02 13:46:31 +0000

    Helpdesk Bcc => CC

--- tests/webuploader/test_notify.py
+++ tests/webuploader/test_notify.py
@@ -97,7 +97,7 @@ class TestNotify(APITestCase):
                 "Subject: Test email subject",
                 "Text: Hi this is just an example",
                 "Queue: " + settings.RT["ena_queue"],
-                "Bcc: " + settings.RT["emg_email"]
+                "CC: " + settings.RT["emg_email"]
             ])
         }
 
