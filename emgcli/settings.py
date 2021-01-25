b10ab4d68fc516ccb949358e8833226a20f26696
Martin Beracochea <mbc@ebi.ac.uk>
2021-01-25 11:37:33 +0000

    Assembly [Study, Coverage, MinGap] values from ENA
    - Assembly model modification
    - Refactor import_analysis (code org and style)
    - import_analysis_model (code org and style)

--- emgcli/settings.py
+++ emgcli/settings.py
@@ -219,11 +219,30 @@ if DEBUG:
     INSTALLED_APPS.extend([
         'django_extensions',
         'debug_toolbar',
+        'debug_toolbar_mongo',
     ])
 
     DEBUG_TOOLBAR_CONFIG = {
         'SHOW_TOOLBAR_CALLBACK': lambda request: True,
     }
+
+    DEBUG_TOOLBAR_PANELS = [
+        # 'debug_toolbar.panels.history.HistoryPanel',
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        'debug_toolbar_mongo.panel.MongoDebugPanel',
+    ]
 
 
 MIDDLEWARE = [
@@ -264,6 +283,8 @@ TEMPLATES = [
         },
     },
 ]
+
+HOSTNAME = "localhost"
 
 WSGI_APPLICATION = 'emgcli.wsgi.application'
 
