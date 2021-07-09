eaf466f481ae5a5660e6c1b591ed7682f3c5d49e
Gustavo A. Salazar <gustavoadolfo.salazar@gmail.com>
2021-07-09 17:23:46 +0100

    returning an estimate of the position in the queue

--- emgapi/sourmash.py
+++ emgapi/sourmash.py
@@ -2,6 +2,8 @@ import json
 from django.conf import settings
 from rest_framework.reverse import reverse
 from celery import Celery, group
+from celery.app.control import Control
+
 import hashlib
 
 
@@ -15,6 +17,7 @@ app = Celery('tasks', broker=settings.SOURMASH['celery_broker'], backend=setting
 app.conf.update(
     result_extended=True
 )
+control = Control(app=app)
 
 
 def validate_sourmash_signature(json_str):
@@ -60,33 +63,69 @@ def send_sourmash_jobs(names, mag_catalog):
     ], app=app)
     result = job.apply_async()
     result.save()
-    children_ids = {
-        r.args[1]: r.id
-        for r in result.results
-    }
-
+    try:
+        children_ids = {
+            r.args[1]: r.id
+            for r in result.results
+        }
+    except Exception:
+        children_ids = None
     return result.id, children_ids
 
 
+# Assumes there is only one worker in the queue
+def get_task_pos_in_reserved(task_id, reserved):
+    pos = 0
+    for tasks in reserved.values():
+        for task in tasks:
+            if task["id"] == task_id:
+                return pos + 1
+            pos += 1
+    return None
+
+
+def get_task_worker_status(task_id, inspect):
+    tasks_by_worker = inspect.query_task(task_id)
+    for task in tasks_by_worker.values():
+        status = task[task_id][0]
+        if status == 'reserved':
+            return "IN_QUEUE"
+        if status == 'active':
+            return "RUNNING"
+
+
 def get_sourmash_job_status(job_id, request):
+    inspect = control.inspect()
+    ping = inspect.ping()
+    reserved = None
     group_result = app.GroupResult.restore(job_id, app=app)
     signatures = []
     for result in group_result.results:
         signature = {
             "job_id": result.id,
             "status": result.status,
-            "filename": result.args[1]
         }
+        try:
+            signature['filename'] = result.args[1]
+        except Exception:
+            pass
         if result.status == 'SUCCESS':
             signature['result'] = result.result
             signature['results_url'] = reverse('genomes-results', args=[result.id], request=request)
-        if result.status == 'FAILURE':
+        elif result.status == 'FAILURE':
             signature['reason'] = str(result.result)
+        elif result.status == 'PENDING' and ping is not None:
+            signature["status"] = get_task_worker_status(result.id, inspect)
+            if signature["status"] == "IN_QUEUE":
+                if reserved is None:
+                    reserved = inspect.reserved()
+                signature['position_in_queue'] = get_task_pos_in_reserved(result.id, reserved)
 
         signatures.append(signature)
     return {
         'group_id': job_id,
-        'signatures': signatures
+        'signatures': signatures,
+        'worker_status': "OFFLINE" if ping is None else "OK"
     }
 
 
