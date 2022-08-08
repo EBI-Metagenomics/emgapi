279364901bade3e795e9225b9f225feecec917c0
Martin Beracochea <mbc@ebi.ac.uk>
2022-08-08 18:19:01 +0100

    Test import contigs - drop entries on tests.
    For some reason this is causing issues on GH Actions.

--- tests/webuploader/test_contigs.py
+++ tests/webuploader/test_contigs.py
@@ -24,6 +24,8 @@ from rest_framework import status
 
 from test_utils.emg_fixtures import *  # noqa
 
+from emgapianns.models import AnalysisJobContig
+
 
 @pytest.mark.django_db
 class TestContigs:
@@ -33,6 +35,8 @@ class TestContigs:
     def test_import_contigs(self, client, run_v5):
         """Run an import contigs and check the results
         """
+        AnalysisJobContig.objects.all().delete()
+
         assert run_v5.accession == 'ABC01234'
         rootpath = os.path.dirname(os.path.abspath(__file__))
         call_command('import_contigs', run_v5.accession, rootpath, '--pipeline', '5.0')
