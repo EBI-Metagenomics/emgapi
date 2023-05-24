40a2dcee40ac59fff8e0168cb169852d40689613
Martin Beracochea <mbc@ebi.ac.uk>
2023-05-24 12:18:26 +0100

    Bump version and upgrade ena-api-handler version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ _base = os.path.dirname(os.path.abspath(__file__))
 _requirements = os.path.join(_base, 'requirements.txt')
 _requirements_test = os.path.join(_base, 'requirements-test.txt')
 
-version = "2.4.20"
+version = "2.4.21"
 
 install_requirements = []
 with open(_requirements) as f:
