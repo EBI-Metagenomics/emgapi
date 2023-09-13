cd0345f5ec458fb0f2e73439b882c611f91ee0e0
Mahfouz <mahfouzshehu@gmail.com>
2023-09-13 11:32:05 +0100

    squashed migrations; added Bulk update

--- emgapianns/management/commands/import_analysis_summaries.py
+++ emgapianns/management/commands/import_analysis_summaries.py
@@ -10,24 +10,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         batch_number = options['batch_number']
-        batch_size = 10000  # Set your desired batch size here
+        batch_size = 10000
 
         try:
-            # Calculate the starting and ending index for the batch
             start_index = (batch_number - 1) * batch_size
             end_index = batch_number * batch_size
 
-            # Get AnalysisJob records for the specified batch
             analysis_jobs = AnalysisJob.objects.all()[start_index:end_index]
 
-            # Print the number of records in the batch
             self.stdout.write(self.style.SUCCESS(f'Processing batch {batch_number} of {len(analysis_jobs)} records.'))
+
+            updated_records = []
 
             for analysis_job in analysis_jobs:
                 analysis_summary = analysis_job.analysis_summary
                 if analysis_summary:
                     analysis_job.analysis_summary_json = analysis_summary
-                    analysis_job.save()
+                    updated_records.append(analysis_job)
+
+            if updated_records:
+                AnalysisJob.objects.bulk_update(updated_records, ['analysis_summary_json'])
 
             self.stdout.write(self.style.SUCCESS(f'Values copied successfully for batch {batch_number}.'))
         except AnalysisJob.DoesNotExist:
