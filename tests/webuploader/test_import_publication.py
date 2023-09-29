8d9a9497386ce39cf5136b3765d05a5d854e16b4
Martin Beracochea <mbc@ebi.ac.uk>
2023-09-29 13:43:03 +0100

    Truncate the fields in the Publication model if longer than expected.
    I've increased the max len of the pub_type in the Publication model.
    To prevent the import to fail in the future I've implemented a brute
    truncation mechanism when a Publication is saved.

--- tests/webuploader/test_import_publication.py
+++ tests/webuploader/test_import_publication.py
@@ -16,28 +16,39 @@
 
 import pytest
 
-from emgapianns.management.commands.import_publication import lookup_publication_by_pubmed_id
+from emgapi.models import Publication
+from model_bakery import baker
 
 
-@pytest.mark.parametrize("pubmed_id, expected_pub_title, expected_year_of_pub, expected_authors, expected_doi", [
-    (4838818,
-     "Proceedings: The morphological variation of nervous structures in the atrial endocardium of the dog.",
-     1974,
-     "Floyd K, Linden RJ, Saunders DA.",
-     "n/a"),
-    (31138692,
-     "Mechanisms by which sialylated milk oligosaccharides impact bone biology in a gnotobiotic mouse "
-     "model of infant undernutrition.",
-     2019,
-     "Cowardin CA, Ahern PP, Kung VL, Hibberd MC, Cheng J, Guruge JL, Sundaresan V, Head RD, Barile D,"
-     " Mills DA, Barratt MJ, Huq S, Ahmed T, Gordon JI.",
-     "10.1073/pnas.1821770116")
-])
-def test_lookup_publication_by_pubmed_id_should_return(pubmed_id,
-                                                       expected_pub_title,
-                                                       expected_year_of_pub,
-                                                       expected_authors,
-                                                       expected_doi):
+from emgapianns.management.commands.import_publication import (
+    lookup_publication_by_pubmed_id,
+)
+
+
+@pytest.mark.parametrize(
+    "pubmed_id, expected_pub_title, expected_year_of_pub, expected_authors, expected_doi",
+    [
+        (
+            4838818,
+            "Proceedings: The morphological variation of nervous structures in the atrial endocardium of the dog.",
+            1974,
+            "Floyd K, Linden RJ, Saunders DA.",
+            "n/a",
+        ),
+        (
+            31138692,
+            "Mechanisms by which sialylated milk oligosaccharides impact bone biology in a gnotobiotic mouse "
+            "model of infant undernutrition.",
+            2019,
+            "Cowardin CA, Ahern PP, Kung VL, Hibberd MC, Cheng J, Guruge JL, Sundaresan V, Head RD, Barile D,"
+            " Mills DA, Barratt MJ, Huq S, Ahmed T, Gordon JI.",
+            "10.1073/pnas.1821770116",
+        ),
+    ],
+)
+def test_lookup_publication_by_pubmed_id_should_return(
+    pubmed_id, expected_pub_title, expected_year_of_pub, expected_authors, expected_doi
+):
     publications = lookup_publication_by_pubmed_id(pubmed_id)
     assert len(publications) == 1
 
@@ -49,18 +60,38 @@ def test_lookup_publication_by_pubmed_id_should_return(pubmed_id,
     assert publication.doi == expected_doi
 
 
-@pytest.mark.parametrize("pubmed_id", [
-    (0),
-    (000)
-])
+@pytest.mark.parametrize("pubmed_id", [(0), (000)])
 def test_lookup_publication_by_pubmed_id_(pubmed_id):
     with pytest.raises(ValueError):
         lookup_publication_by_pubmed_id(pubmed_id)
 
 
-@pytest.mark.parametrize("pubmed_id", [
-    ("test")
-])
+@pytest.mark.parametrize("pubmed_id", [("test")])
 def test_lookup_publication_by_pubmed_id_raises_exception_on_string(pubmed_id):
     with pytest.raises(TypeError):
         lookup_publication_by_pubmed_id(pubmed_id)
+
+
+@pytest.mark.django_db
+def test_text_fields_longer_than_expected(faker):
+    PUB_TITLE_MAX = 740
+    PUB_TYPE_MAX = 300
+    VOLUME_MAX = 55
+
+    # I've picked 3 fields as representatives
+    publications = baker.prepare(
+        Publication,
+        pub_title=faker.text(max_nb_chars=PUB_TITLE_MAX + 1000),
+        pub_type=faker.text(max_nb_chars=PUB_TYPE_MAX + 1000),
+        volume=faker.text(max_nb_chars=VOLUME_MAX + 1000),
+        _quantity=5,
+    )
+
+    for publication in publications:
+        assert len(publication.pub_title) > PUB_TITLE_MAX
+        assert len(publication.pub_type) > PUB_TYPE_MAX
+        assert len(publication.volume) > VOLUME_MAX
+        publication.save()
+        assert len(publication.pub_title) <= PUB_TITLE_MAX
+        assert len(publication.pub_type) <= PUB_TYPE_MAX
+        assert len(publication.volume) <= VOLUME_MAX
