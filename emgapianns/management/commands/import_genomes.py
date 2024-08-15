d6f2014b2286bbd2e1a237a82b56b96d2ad4c280
Tatiana Gurbich <tgurbich@gmail.com>
2024-08-15 11:38:53 +0100

    Added new file types

--- emgapianns/management/commands/import_genomes.py
+++ emgapianns/management/commands/import_genomes.py
@@ -387,6 +387,15 @@ class Command(BaseCommand):
                                 genome.accession + '_crisprcasfinder.tsv', 'Genome analysis', 'genome', False)
         self.upload_genome_file(genome, directory, 'Genome Mobilome Annotation', 'gff',
                                 genome.accession + '_mobilome.gff', 'Genome analysis', 'genome', False)
+        # pipeline v2.4.0 files (if not empty):
+        self.upload_genome_file(genome, directory, 'Genome dbCAN annotation', 'gff',
+                                genome.accession + '_dbcan.gff', 'Genome analysis', 'genome', False)
+        self.upload_genome_file(genome, directory, 'Genome Defense Finder annotation', 'gff',
+                                genome.accession + '_defense_finder.gff', 'Genome analysis', 'genome', False)
+        self.upload_genome_file(genome, directory, 'Genome GECCO annotation', 'gff',
+                                genome.accession + '_gecco.gff', 'Genome analysis', 'genome', False)
+        self.upload_genome_file(genome, directory, 'KEGG pathway completeness', 'tsv',
+                                genome.accession + '_kegg_pathways.tsv', 'Genome analysis', 'genome', False)
 
         if has_pangenome:
             self.upload_genome_file(genome, directory, 'Pangenome core genes list', 'tab',
